db9ce9f70d4c8dc3b7e01d04102477b9807cbef3
José Cartaxo <135115268+Jose-Cartaxo@users.noreply.github.com>
2024-07-29 14:17:43 +0100

    toque mais resultados KNNNDBSCAN

--- Teste.py
+++ Teste.py
@@ -68,7 +68,7 @@ def knna():
     dbscan_min = -1
     dbscan_max = -1
     dbscan_ite = -1
-
+    
     lista_knn_ite = [2,3,4,5,6,7,8,9,10,11,12]
     tempo_dep = 60
     tempo_ant = 30
@@ -115,11 +115,14 @@ def knndbscan():
     # 3 - K-NearestNeighbors com DBSCANS
     metodoCluster = 3
 
-    lista_dbscan_min = [1,2,3,4,5]
-    lista_dbscan_max = [10,15,20,25,30,35,40]
+    # lista_dbscan_min = [1,2,3,4,5]
+    lista_dbscan_min = [5]
+    # lista_dbscan_max = [10,15,20,25,30,35,40]
+    lista_dbscan_max = [35,40]
     lista_dbscan_ite = [1,2,3,4,5]
 
-    lista_knn_ite = [12]
+    # lista_knn_ite = [2,3,4,5,6,7,8,9,10,11,12]
+    lista_knn_ite = [7]
     tempo_dep = 60
     tempo_ant = 30
 
@@ -266,8 +269,8 @@ def central():
 
 
 
-knnn()
-knna()
-# knndbscan()
+# knnn()
+# knna()
+knndbscan()
 # dbscan()
 # central()
